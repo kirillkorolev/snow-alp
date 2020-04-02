45b390f539679cc54bd22fae4decaad8d2f15193
Кирилл <kkirillcore@gmail.com>
2020-04-03 01:11:26 +0300

    добавляет другие блоки сайта

--- build/js/mask.js
+++ build/js/mask.js
@@ -1,7 +1,13 @@
-$(function() {
-  $("#phone").mask("+7(999) 999-99-99");
+'use strict';
+
+$(function () {
+  $('#phone').mask('+7(999) 999-99-99');
 });
 
-$(function() {
-  $("#call-back-phone").mask("+7(999) 999-99-99");
+$(function () {
+  $('#call-back-phone').mask('+7(999) 999-99-99');
+});
+
+$(function () {
+  $('#call-request-call').mask('+7(999) 999-99-99');
 });
